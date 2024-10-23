704257bab947134652fd17b7e9224fb9a0087ae8
emanuelginer <emanuelginer77@gmail.com>
2024-10-23 09:44:27 -0300

    Change how the ids were selected

--- src/components/useFetchPokemons.jsx
+++ src/components/useFetchPokemons.jsx
@@ -2,18 +2,20 @@ import { useEffect, useState } from "react";
 
 const useFetchPokemons = (limit) => {
 const [ arrayOfPokemons, setArrayOfPokemons] = useState([]);
+let differentPokemonId = 0
 
   useEffect(() => {
     const fetchData = async() => {
       try {
         let pokemons = [];
         for (let i = 1; i <= limit; i++) {
-          const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+          differentPokemonId += 10
+          const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${differentPokemonId}`);
           if(!response.ok) {
             throw new Error('Network response was not ok');
           }
           const data = await response.json();
-          pokemons.push({img: data.sprites.front_default, name: data.name, id: i});
+          pokemons.push({img: data.sprites.front_default, name: data.name, id: differentPokemonId});
         }
         setArrayOfPokemons(pokemons)
       } catch (error) {
