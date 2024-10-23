4ac5859661b8bde407ae6d91b191963ff502a17a
emanuelginer <emanuelginer77@gmail.com>
2024-10-23 09:45:29 -0300

    Fix a typo, add score and bestScore logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import './styles/main.css'
 import './styles/App.css'
 import useFetchPokemons from './components/useFetchPokemons';
 import Card from './components/Card';
+import Header from './components/Header';
 
 
 
 function App() {
   const arrayOfPokemons = useFetchPokemons(10);
   const [ sortedArrayOfPokemons, setSortedArrayOfPokemons] = useState([]);
+  const [ repeteadId, setRepeteadId ] = useState([]);
+  const [ score, setScore ] = useState(0);
+  const [ bestScore, setBestScore ] = useState(0);
+
 
   const shuffle = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
@@ -25,28 +30,38 @@ function App() {
     }
   }, [arrayOfPokemons]);
 
-  const handleClick = () => {
+  const handleClick = (id) => {
+    if(repeteadId.includes(id)) {
+      setScore(0)
+      setRepeteadId([])
+    } else {
+      const newScore = score + 10
+      setScore(newScore)
+      setRepeteadId([...repeteadId, id])
+      setBestScore((prevBestScore) => {
+        return newScore > prevBestScore ? newScore : prevBestScore;
+      });
+    }
     shuffle(sortedArrayOfPokemons)
   }
 
     return (
     <>
-      <div>
-        <h1>Memory Game</h1>
-        <p>Score: </p>
-        <p>Best Score:</p>
-      </div>
+      <Header
+        score = { score }
+        bestScore = { bestScore }
+      />
       <div className="cards">
-        {sortedArrayOfPokemons.length === 0
-        ? <p>Is loading...</p>
-        :  sortedArrayOfPokemons.map((pokemon) => (
-            <Card 
-              key = { pokemon.id } 
-              pokeImg = { pokemon.img }  
-              name = { pokemon.name }
-              onClick = { handleClick }
-            />
-          ))}
+      {sortedArrayOfPokemons.length === 0
+      ? <p>Is loading...</p>
+      :  sortedArrayOfPokemons.map((pokemon) => (
+          <Card 
+            key = { pokemon.id } 
+            pokeImg = { pokemon.img }  
+            name = { pokemon.name }
+            onClick = { () => handleClick(pokemon.id) }
+          />
+        ))}
       </div>
     </>
   )
